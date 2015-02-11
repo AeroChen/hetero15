edd36031444b6b5f3255c04b3692a97568890d0b
Xichen Shi <sxc901231@gmail.com>
2015-02-11 01:36:19 -0600

    MP6 (Image convolution) accepted solution, notice TILE_WIDTH not too exceed max threads allowed per block

--- skel/mp6.cu
+++ skel/mp6.cu
@@ -11,9 +11,45 @@
 
 #define Mask_width  5
 #define Mask_radius Mask_width/2
+#define O_TILE_WIDTH 12
+#define BLOCK_WIDTH O_TILE_WIDTH+Mask_width-1
 
 //@@ INSERT CODE HERE
+__global__ void convolution_2D(float* input, float* output, int height, int width, int channels, const float* __restrict__ mask) {
+	__shared__ float ds_input[BLOCK_WIDTH][BLOCK_WIDTH][3];
 
+	// Output index map
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int tz = threadIdx.z;
+	int row_o = blockIdx.y * O_TILE_WIDTH + ty;
+	int col_o = blockIdx.x * O_TILE_WIDTH + tx;
+	// Input index map
+	int row_i = row_o - Mask_radius;
+	int col_i = col_o - Mask_radius;
+
+	if ((row_i >= 0) && (row_i < height) && (col_i >= 0) && (col_i < width)) {
+		ds_input[ty][tx][tz] = input[(row_i*width + col_i) * channels + tz];
+	}
+	else {
+		ds_input[ty][tx][tz] = 0.0f;
+	}
+	__syncthreads();
+
+	if (tx < O_TILE_WIDTH && ty < O_TILE_WIDTH) {
+		float result = 0.0f;
+		for (int i = 0; i < Mask_width; i++)
+		{
+			for (int j = 0; j < Mask_width; j++)
+			{
+				result += mask[i*Mask_width + j] * ds_input[ty + i][tx + j][tz];
+			}
+		}
+		if (row_o < height && col_o < width) {
+			output[(row_o*width + col_o) * channels + tz] = min(max(result, 0.0f), 1.0f);
+		}
+	}
+}
 
 int main(int argc, char* argv[]) {
 	wbArg_t arg;
@@ -76,6 +112,9 @@ int main(int argc, char* argv[]) {
 
 	wbTime_start(Compute, "Doing the computation on the GPU");
 	//@@ INSERT CODE HERE
+	dim3 dimGrid((imageWidth - 1) / O_TILE_WIDTH + 1, (imageHeight - 1) / O_TILE_WIDTH + 1, 1);
+	dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH, imageChannels);
+	convolution_2D<<<dimGrid,dimBlock>>>(deviceInputImageData, deviceOutputImageData, imageHeight, imageWidth, imageChannels, deviceMaskData);
 	wbTime_stop(Compute, "Doing the computation on the GPU");
 
 
